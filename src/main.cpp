616b7aded7ab01ecdec3150a4930f3f49154568e
alexshidagoatnocap <115957532+alexshidagoatnocap@users.noreply.github.com>
2026-02-20 18:18:47 -0600

    feat: added sht30 readings

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,10 @@
+#include "HardwareSerial.h"
 #include <Adafruit_BMP085.h>
+#include <Adafruit_SHT31.h>
 #include <Arduino.h>
 
-/***************************************************
-  This is an example for the BMP085 Barometric Pressure & Temp Sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout
-  ----> https://www.adafruit.com/products/391
-
-  These pressure and temperature sensors use I2C to communicate, 2 pins
-  are required to interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
-// Connect GND to Ground
-// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats
-// Analog 5 Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc
-// thats Analog 4 EOC is not used, it signifies an end of conversion XCLR is a
-// reset pin, also not used here
-
 Adafruit_BMP085 bmp;
+Adafruit_SHT31 sht3x;
 
 void setup() {
   Serial.begin(9600);
@@ -33,24 +13,30 @@ void setup() {
     while (1) {
     }
   }
+
+  if (!sht3x.begin(0x44)) {
+    Serial.println("Could not find SHT30");
+    while (1) {
+    }
+  }
 }
 
 void loop() {
-  Serial.print("Temperature = ");
+  Serial.print("BMP085 Temperature = ");
   Serial.print(bmp.readTemperature());
   Serial.println(" *C");
 
-  Serial.print("Pressure = ");
+  Serial.print("BMP085 Pressure = ");
   Serial.print(bmp.readPressure());
   Serial.println(" Pa");
 
   // Calculate altitude assuming 'standard' barometric
   // pressure of 1013.25 millibar = 101325 Pascal
-  Serial.print("Altitude = ");
+  Serial.print("BMP085 Altitude = ");
   Serial.print(bmp.readAltitude());
   Serial.println(" meters");
 
-  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print("BMP085 Pressure at sealevel (calculated) = ");
   Serial.print(bmp.readSealevelPressure());
   Serial.println(" Pa");
 
@@ -58,9 +44,17 @@ void loop() {
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
-  Serial.print("Real altitude = ");
+  Serial.print("BMP085 Real altitude = ");
   Serial.print(bmp.readAltitude(101500));
   Serial.println(" meters");
+
+  Serial.print("SHT30 Temperature = ");
+  Serial.print(sht3x.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("SHT30 Humidity = ");
+  Serial.print(sht3x.readHumidity());
+  Serial.println(" %");
 
   Serial.println();
   delay(500);
